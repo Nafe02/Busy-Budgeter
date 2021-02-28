dd6522b0957939fefaecc44abcb53aee97f0fee2
Nafe02 <40699164+Nafe02@users.noreply.github.com>
2021-02-28 18:35:43 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -85,14 +85,14 @@ class UI {
     div.innerHTML = `<div class="expense-item d-flex justify-content-between align-items-baseline">
 
     <h6 class="expense-title mb-0 text-uppercase list-item">- ${expense.title}</h6>
-    <h5 class="expense-amount mb-0 list-item">$${expense.amount}</h5>
+    <h5 class="expense-amount mb-0 list-item">&#8358;${expense.amount}</h5>
 
     <div class="expense-icons list-item">
 
-     <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+     <a href="#" class="edit-icon mx-2" data-id="&#8358;${expense.id}">
       <i class="fas fa-edit"></i>
      </a>
-     <a href="#" class="delete-icon" data-id="${expense.id}">
+     <a href="#" class="delete-icon" data-id="&#8358;${expense.id}">
       <i class="fas fa-trash"></i>
      </a>
     </div>
